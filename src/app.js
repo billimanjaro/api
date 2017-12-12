b067525be63b03fecbd822e8c6e25c598f011e52
Jeff Hubbard <lord2800@gmail.com>
2017-12-11 19:31:30 -0800

    Use morgan and provide a selector for log profile

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const app = express.Router();
 
 app.use(require('cors')());
 app.use(express.json());
+app.use(require('morgan')(process.env.LOG_PROFILE || 'dev'));
 
 ['v1'].map(version => app.use('/' + version, require('./' + version)));
 
