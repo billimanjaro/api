2b52f05c9d31e3d5cb04197599186e9e43e144fc
Jeff Hubbard <lord2800@gmail.com>
2017-12-11 16:16:01 -0800

    Enable CORS support

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express.Router();
 
+app.use(require('cors')());
+
 ['v1'].map(version => app.use('/' + version, require('./' + version)));
 
 module.exports = app;
